8f6a7263240e307873fd031a9dc803f3165efa83
caijian <825388643@qq.com>
2018-07-06 10:10:13 +0800

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const service = axios.create({
-    baseURL:'http://192.168.240.44:3000/',
+    baseURL:'http://118.25.100.171:3000',
     timeout:5000,
 })
 //添加请求拦截器
